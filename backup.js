48fe8d85dc870b8de614f2e9d84fbd456248ec51
Renick Büttner <renick@staffbase.com>
2020-04-14 11:46:09 +0200

    increase exec() buffer

--- backup.js
+++ backup.js
@@ -78,7 +78,7 @@ async function _backup(path, fileName) {
 function execShellCommand(cmd) {
     const exec = require('child_process').exec;
     return new Promise((resolve, reject) => {
-        exec(cmd, (error, stdout, stderr) => {
+        exec(cmd, {maxBuffer: 1024 * 1000 * 128}, (error, stdout, stderr) => {
             if (error) {
                 console.warn(error);
             }
